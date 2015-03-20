35f4c1a21daacaefe5fd3df4c1f1a421656301f7
wiwang <william.calix.cn@gmail.com>
2015-03-20 02:41:51 -0700

    BiThrTree

---
+++ Tree/BiThrTree.c
@@ -0,0 +1,5 @@
+#include "Base.h"
+#include "BiTree.h"
+
+
+
--- Tree/BiTree.h
+++ Tree/BiTree.h
@@ -6,3 +6,18 @@ typedef struct BinTree
     BinTree* lchild;
     BinTree* rchild;
 }BinTree;
+
+typedef enum
+{
+    Link,
+    Thread
+}PointerTag;
+
+typedef struct BiThrNode
+{
+    Elem data;
+    struct BiThrNode *lchild;
+    struct BiThrNode *rchild;
+    PointerTag ltag;
+    PointerTag rtag;
+}
