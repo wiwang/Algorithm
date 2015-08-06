874edbfd3d2c655cc0c76018297dbca426f5373d
william wang <william.wang@calix.com>
2015-08-05 21:24:21 -0500

    update BiTree

--- Tree/BiTree.c
+++ Tree/BiTree.c
@@ -4,34 +4,34 @@
 void RecurPreOrderTraverse(BinTree *root)
 {
     if(root != NULL)
-	{
-	    printf("%d.\n", root->data);
-	    PreOrderTraverse(root->lchild);
-		PreOrderTraverse(root->rchild);
-	}
+    {
+        printf("%d.\n", root->data);
+	PreOrderTraverse(root->lchild);
+        PreOrderTraverse(root->rchild);
+    }
 }
 
 
 void NonRecurPreOrderTraverse(BinTree *root)
 {
     stack<BinTree *> s;
-	BinTree *p = root;
-	while(p!=NULL || !s.empty())
-	{
-	    while(p!=NULL)
-		{
-		    printf("%d.\n", p->data);
-			s.push(p);
-			p = p->lchild;		
-		}
-		
-		while(!s.empty())
-		{
-		    p = s.top();
-			s.pop();
-			p = p->rchild;
-		}	
+    BinTree *p = root;
+    while(p!=NULL || !s.empty())
+    {
+	while(p!=NULL)
+        {
+            printf("%d.\n", p->data);
+	    s.push(p);
+	    p = p->lchild;		
 	}
+		
+	if(!s.empty())
+	{
+	    p = s.top();
+	    s.pop();
+	    p = p->rchild;
+	}	
+    }
 }
 
 
@@ -39,44 +39,44 @@ void NonRecurPreOrderTraverse(BinTree *root)
 void RecurInOrderTraverse(BinTree *root)
 {
     if(root != NULL)
-	{
-	    RecurInOrderTraverse(root->lchild);
-	    printf("%d.\n", root->data);		
-		RecurInOrderTraverse(root->rchild);
-	}
+    {
+	RecurInOrderTraverse(root->lchild);
+	printf("%d.\n", root->data);		
+        RecurInOrderTraverse(root->rchild);
+    }
 }
 
 void NonRecurInOrderTraverse(BinTree *root)
 {
     stack<BinTree *> s;
-	BinTree *p = root;
-	while(p!=NULL || !s.empty())
+    BinTree *p = root;
+    while(p!=NULL || !s.empty())
+    {
+	while(p!=NULL)
 	{
-	    while(p!=NULL)
-		{
-			s.push(p);
-			p = p->lchild;		
-		}
-		
-		while(!s.empty())
-		{
-		    p = s.top();
-		    printf("%d.\n", p->data);			
-			s.pop();
-			p = p->rchild;
-		}	
+	    s.push(p);
+	    p = p->lchild;		
 	}
+		
+	if(!s.empty())
+	{
+	    p = s.top();
+	    printf("%d.\n", p->data);			
+	    s.pop();
+	    p = p->rchild;
+	}	
+    }
 }
 
 
 void RecurPostOrderTraverse(BinTree *root)
 {
     if(root != NULL)
-	{
-	    RecurInOrderTraverse(root->lchild);		
-		RecurInOrderTraverse(root->rchild);
-	    printf("%d.\n", root->data);		
-	}
+    {
+	RecurPostOrderTraverse(root->lchild);		
+        RecurPostOrderTraverse(root->rchild);
+	printf("%d.\n", root->data);		
+    }
 }
 
 void NonRecurPostOrderTraverse(BinTree *root)
